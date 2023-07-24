43c39eaaf163c4dead14069ced7a982507b05f75
Karan Maheshwari <63113271+mkaran15@users.noreply.github.com>
2023-07-24 08:05:39 +0000

    DataTypes

---
+++ DataTypes and Conversion/datatype.js
@@ -0,0 +1,16 @@
+let value = 2; // number
+console.log(typeof value); 
+
+let value1 = "Karan Maheshwari"; //string
+console.log(typeof value1); 
+
+let value2 = "true"; //string
+let value3 = true; // boolean
+console.table([typeof value2, typeof value3]);
+
+let value4 = 23.76; //number
+console.log(typeof value4);
+
+let value5= null; // object
+let value6 = undefined; //undefined
+console.table([typeof value5, typeof value6]);
