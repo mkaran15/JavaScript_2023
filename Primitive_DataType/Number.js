4fc6c0f738db1fc70704130ea55a029607b840e2
Karan Maheshwari <63113271+mkaran15@users.noreply.github.com>
2023-07-26 10:34:26 +0000

    Numbers in JS

---
+++ Primitive_DataType/Number.js
@@ -0,0 +1,12 @@
+//++++++++++++ Creating String ++++++++++++
+let age = 18;
+let newAge = new Number(18);
+console.log(age); // 18
+console.log(typeof age); //number
+console.log(newAge); // [Number: 18]
+console.log(typeof newAge); //object
+
+//MAX_VALUE and MIN_VALUE
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_VALUE);
+
